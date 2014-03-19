0b2c0d142dff8895189ad422edcc9d1a93b31a6a
Peter Mescalchin <peter@magnetikonline.com.au>
2014-03-19 12:42:23 +1100

    Bash variable rename in cleanup utility script

--- thummercleanup.sh
+++ thummercleanup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-BASESOURCEDIR="/webapp/docroot/content/image"
-BASETARGETDIR="/webapp/docroot/content/imagethumb";
+BASE_SOURCE_DIR="/webapp/docroot/content/image"
+BASE_TARGET_DIR="/webapp/docroot/content/imagethumb";
 
 
 # fetch all thumbnail images
-for IMAGETHUMBNAIL in `find "$BASETARGETDIR" -type f \( -name "*.gif" -or -name "*.jpg" -or -name "*.jpeg" -or -name "*.png" \)`
+for imageThumbnail in `find "$BASE_TARGET_DIR" -type f \( -name "*.gif" -or -name "*.jpg" -or -name "*.jpeg" -or -name "*.png" \)`
 do
 	# strip thumbnail path and WxH component
-	IMAGESOURCE=${IMAGETHUMBNAIL#$BASETARGETDIR/}
-	if [[ "$IMAGESOURCE" =~ ^[0-9]{1,3}x[0-9]{1,3}(/.+)$ ]]; then
-		IMAGESOURCE=$BASESOURCEDIR${BASH_REMATCH[1]}
+	imageSource=${imageThumbnail#$BASE_TARGET_DIR/}
+	if [[ "$imageSource" =~ ^[0-9]{1,3}x[0-9]{1,3}(/.+)$ ]]; then
+		imageSource=$BASE_SOURCE_DIR${BASH_REMATCH[1]}
 
-		if [ -f "$IMAGESOURCE" ]; then
-			if [ `stat -c %Y "$IMAGETHUMBNAIL"` != `stat -c %Y "$IMAGESOURCE"` ]; then
+		if [ -f "$imageSource" ]; then
+			if [ `stat -c %Y "$imageThumbnail"` != `stat -c %Y "$imageSource"` ]; then
 				# thumbnail modification time does not equal that of source
-				rm -f "$IMAGETHUMBNAIL"
+				rm -f "$imageThumbnail"
 			fi
 		else
 			# source image no longer exists
-			rm -f "$IMAGETHUMBNAIL"
+			rm -f "$imageThumbnail"
 		fi
 	fi
 done
